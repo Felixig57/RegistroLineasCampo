281db2662f2a78494e8dfc13783f280dde8fec47
DanielDev <felixig57gmail.com>
2025-12-13 16:57:30 -0700

    Programacion de la navegacion

--- app/src/main/java/com/example/registrolineascampo/Configuracion.java
+++ app/src/main/java/com/example/registrolineascampo/Configuracion.java
@@ -1,24 +1,43 @@
 package com.example.registrolineascampo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 public class Configuracion extends AppCompatActivity {
+    private Button btnNavConfig, btnNavRegistro,btnNavReportes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_configuracion);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+      //Inicializar Componentes
+        btnNavConfig = findViewById(R.id.btnNavConfig);
+        btnNavRegistro = findViewById(R.id.btnNavRegistro);
+        btnNavReportes = findViewById(R.id.btnNavReportes);
+        //Termina inicializacion
+        //Inicializar Metodos
+        btnNavReportes.setOnClickListener(View -> NavReportes());
+        btnNavRegistro.setOnClickListener(View -> NavRegistro());
+    }
+    private void NavRegistro(){
+        if (btnNavRegistro.isClickable()){
+            Intent PantallaRegistro= new Intent(Configuracion.this, Registros.class);
+            startActivity(PantallaRegistro);
+            finish();
+        }
+    }
+    private void NavReportes(){
+        if(btnNavReportes.isClickable()){
+            Intent PantallaReportes = new Intent(Configuracion.this, Reportes.class);
+            startActivity(PantallaReportes);
+            finish();
+        }
     }
 }
